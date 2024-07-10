4f2e0f6143616b2f5f2e09ddc8bb6809e57d3bcd
Asik8 <motasembillah84@gmail.com>
2024-07-10 23:07:44 +0600

    3 done

---
+++ Reversing_a_Queue.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h> 
+queue<int> reverseQueue(queue<int> q)
+{
+    stack <int>st;
+    while(!q.empty())
+    {
+        st.push(q.front());
+        q.pop();
+    }
+    while(!st.empty())
+    {
+        q.push(st.top());
+        st.pop();
+    }
+    return q;
+}
